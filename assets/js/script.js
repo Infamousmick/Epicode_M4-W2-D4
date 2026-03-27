a69d1674a1f08ac2e5fcacedcb78a27d01c4722c
Infamousmick <mmarchese6690@gmail.com>
2026-03-27 21:08:54 +0100

    update

--- assets/js/script.js
+++ assets/js/script.js
@@ -38,6 +38,9 @@
 
 // . Crea un pulsante per svuotare il carrello
 
+let allBooks = [];
+let cart = [];
+
 const getBooks = async (url) => {
   try {
     const response = await fetch(url);
@@ -47,30 +50,54 @@ const getBooks = async (url) => {
   }
 };
 
-const loadBooks = async () => {
+const init = async () => {
   let url = "https://striveschool-api.herokuapp.com/books";
-  const data = await getBooks(url);
-  const books = formatBooksData(data);
+  allBooks = await getBooks(url);
+  const books = formatBooksData(allBooks);
   createBookCards(books);
 };
 
-loadBooks();
+init();
+
+const input = document.querySelector(".searchContainer .inputBox");
+input.addEventListener("input", () => {
+  checkInputLength(input.value.trim(), 3);
+});
+
+const checkInputLength = (searchInput, num) => {
+  if (searchInput.length >= num) {
+    const filteredData = filterData(allBooks, searchInput);
+    createBookCards(formatBooksData(filteredData));
+  } else {
+    const books = formatBooksData(allBooks);
+    createBookCards(books);
+  }
+};
+
+const filterData = (data, searchInput) => {
+  return data.filter((book) => {
+    return book.title.toLowerCase().includes(searchInput.toLowerCase());
+  });
+};
 
 const formatBooksData = (arrBooks) => {
   return arrBooks
     .map((book) => {
-      const { img, price, title } = book;
+      const { asin, img, price, title } = book;
       return `
         <div class="col">
-          <div class="card">
-            <img src="${img}" class="card-img-top" alt="Book Image"/>
-        <div class="card-body">
-          <h5 class="card-title">${title}</h5>
-          <p class="card-text">${price}</p>
-          <a href="#" class="btn btn-primary"><i class="fa-solid fa-cart-shopping"></i>Add to cart</a>
-          <a href="#" class="btn btn-danger"><i class="fa-regular fa-circle-xmark"></i>Remove</a></div>
-        </div>
-      </div>`;
+          <div class="card h-100">
+            <img src="${img}" class="object-fit-cover rounded-2" style="height: 200px" alt="Book Image"/>
+             <div class="card-body d-flex flex-column">
+              <h5 class="card-title">${title}</h5>
+              <p class="card-text">${price}</p>
+                <div class="d-flex flex-column gap-2 mt-auto">          
+                  <a href="#" class="btn btn-primary add-to-cart" data-asin="${asin}"><i class="fa-solid fa-cart-shopping"></i>Add to cart</a>
+                  <a href="#" class="btn btn-danger hide"><i class="fa-regular fa-circle-xmark"></i>Hide</a>
+                </div>
+              </div>
+            </div>
+          </div>`;
     })
     .join("\n");
 };
@@ -78,4 +105,57 @@ const formatBooksData = (arrBooks) => {
 const createBookCards = (books) => {
   const container = document.querySelector("section#books .booksContainer");
   container.innerHTML = books;
+  assignBtnCarts();
+  assignBtnRemove();
+};
+
+const assignBtnCarts = () => {
+  addToCartBtn = document.querySelectorAll("section#books .add-to-cart");
+  addToCartBtn.forEach((btn) => {
+    btn.addEventListener("click", (e) => {
+      e.preventDefault();
+
+      const clickedBtn = e.target.closest(".add-to-cart");
+      clickedBtn.classList.add("btn-success");
+      clickedBtn.classList.remove("btn-primary");
+
+      const bookAsin = clickedBtn.dataset.asin;
+      cart.push(allBooks.find((book) => book.asin.includes(bookAsin)));
+
+      const card = e.target.closest(".card");
+      card.classList.add("class", "checked");
+
+      clickedBtn.querySelector("i").classList.remove("fa-cart-shopping");
+      clickedBtn.querySelector("i").classList.add("fa-check");
+      injectBooksModal();
+    });
+  });
+};
+
+const assignBtnRemove = () => {
+  btnRemove = document.querySelectorAll("section#books .hide");
+  btnRemove.forEach((btn) => {
+    btn.addEventListener("click", (e) => {
+      e.preventDefault();
+      const card = e.target.closest(".card");
+      card.remove();
+    });
+  });
+};
+
+const injectBooksModal = () => {
+  const modal = document.querySelector("#booksModal .modal-body");
+  let li = "";
+  cart.forEach((item) => {
+    const { asin, title, price } = item;
+    li += `
+    <li class="list-group-item">
+      <h5>${title}
+      </h5>
+      <p>${price}</p>
+      <a class="btn btn-danger remove" data-asin=${asin}><i class="fa-regular fa-circle-xmark"></i>Remove</a>
+    </li>
+`;
+  });
+  modal.innerHTML = `<ul class="list-group">${li}</ul>`;
 };
